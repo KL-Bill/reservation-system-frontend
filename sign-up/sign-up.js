a79d4be8307d6dae63904c685f03d5b1a38a61a7
bebot <kimjeebillones@gmail.com>
2026-06-02 17:13:37 +0800

    try daw

---
+++ sign-up/sign-up.js
@@ -0,0 +1,25 @@
+ const signupBtn = document.getElementById('signupBtn')
+
+ signupBtn.addEventListener('click', () => {
+     const clientId = document.getElementById('id')
+     const clientName = document.getElementById('name')
+     const clientUserName = document.getElementById('userName')
+     const clientPassword = document.getElementById('password')
+
+     saveClient(clientId.value, clientName.value, clientUserName.value, clientPassword.value)
+})
+
+function saveClient(id, name, username, password){
+    axios.post('http://localhost:3000/signup', {
+        id,
+        name,
+        username,
+        password
+    })
+    .then(function (response) {
+        alert(response.data.message)
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+}
